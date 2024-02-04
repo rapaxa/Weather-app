f502772d591f397080d151c6aa33b0746ccd0380
pavelvolodcenko <rapaxa123@gmail.com>
2024-02-04 11:37:44 +0000

    first commit

---
+++ src/api/WeatherData.ts
@@ -0,0 +1,14 @@
+export const fetchData = async (url: string) => {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('City not found');
+        } else {
+            return await response.json();
+        }
+
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
---
+++ src/types/types.ts
@@ -0,0 +1,53 @@
+export interface CurrentConditions {
+    datetime: string;
+    temp: number;
+    feelslike: number;
+    humidity: number;
+    dew: number;
+    precip: number;
+    precipprob: number;
+    snow: number;
+    snowdepth: number;
+    windgust: number;
+    windspeed: number;
+    winddir: number;
+    pressure: number;
+    visibility: number;
+    cloudcover: number;
+    solarradiation: number;
+    solarenergy: number;
+    uvindex: number;
+    conditions: string;
+    icon: string;
+}
+
+export interface WeatherData {
+    queryCost: number;
+    latitude: number;
+    longitude: number;
+    resolvedAddress: string;
+    address: string;
+    timezone: string;
+    tzoffset: number;
+    description: string;
+    days: Array<Day>;
+    // Define this more specifically if needed
+    currentConditions: CurrentConditions;
+}
+
+export interface Day {
+    tempmax: number,
+    tempmin: number,
+    datetime:string,
+    conditions:string,
+    feelslikemax: number,
+    feelslikemin: number,
+    visibility: number,
+    icon: string
+    hours:Array<Hours>
+}
+export interface Hours {
+    datetime:string,
+    temp:number,
+    icon:string
+}
